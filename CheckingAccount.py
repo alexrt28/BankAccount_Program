6cec1b10231fe6b6093fb14260c9aa1aa8b6e35f
Julian Dominguez <jdominguez9@gmail.com>
2026-02-18 16:54:32 -0500

    feat: Add Assignment Scenario

--- CheckingAccount.py
+++ CheckingAccount.py
@@ -1,8 +1,8 @@
-import BankAccount
+from BankAccount import BankAccount
 
 class CheckingAccount(BankAccount):
     def __init__(self,customer_name, current_balance, minimum_balance, transfer_limit):
-        super.__init__(customer_name,current_balance,minimum_balance)
+        super().__init__(customer_name,current_balance,minimum_balance)
         self.transfer_limit = transfer_limit
 
     def transfer(self, amount, destination_account):
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from CheckingAccount import CheckingAccount
 from SavingsAccount import SavingsAccount
 
-def main():
+checking1 = CheckingAccount("Johnathon", 5000, 500, 400)
+checking2 = CheckingAccount("Miriam", 12000, 1000, 5000)
 
-    checking1 = CheckingAccount("Johnathon", 5000, 500, 400)
-    checking2 = CheckingAccount("Miriam", 12000, 1000, 5000)
+savings1 = SavingsAccount("Emilia", 20000, 3000, 0.05)
+savings2 = SavingsAccount("Walton", 16000, 1000, 0.04)
 
-    savings1 = SavingsAccount("Emilia", 20000, 3000, 0.05)
-    savings2 = SavingsAccount("Walton", 16000, 1000, 0.04)
-
-    
+print(" ==== Assignment Scenario below: ===")
+checking1.deposit(500)
+checking1.withdraw(100)
+checking1.transfer(500, savings2)   #will fail because $500 > $400 transfer limit
+print(" === End Assignment Scenario. ===")
