485b547e32d6107216289bc9e228e7b8e4ae9ca2
Julian Dominguez <jdominguez9@gmail.com>
2026-02-18 16:25:52 -0500

    feat: add savings account creation in main

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ def main():
     checking1 = CheckingAccount("Johnathon", 5000, 500, 400)
     checking2 = CheckingAccount("Miriam", 12000, 1000, 5000)
 
+    savings1 = SavingsAccount("Emilia", 20000, 3000, 0.05)
+    savings2 = SavingsAccount("Walton", 16000, 1000, 0.04)
 
+    
