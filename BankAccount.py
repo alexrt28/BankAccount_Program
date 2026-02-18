273ebb0ab452fac5200ec6774196f94bea76b9c1
Alex Roy <alexroy.sebastian@gmail.com>
2026-02-18 18:25:57 -0500

    feat: Created protected and private members in SavingsAccount

--- BankAccount.py
+++ BankAccount.py
@@ -3,10 +3,15 @@ class BankAccount:
     bank_title = "Platinum Shield Bank"
 
     #instance attributes
-    def __init__(self, customer_name, current_balance, minimum_balance):
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number):
         self.customer_name = customer_name
         self.current_balance = current_balance
         self.minimum_balance = minimum_balance
+
+        #protected member
+        self.account_number = account_number
+        #private member
+        self.routing_number = routing_number
 
     #deposit method
     def deposit(self, amount):
@@ -30,26 +35,8 @@ class BankAccount:
         print("----- Customer Information -----")
         print(f"Bank: {BankAccount.bank_title}")
         print(f"Customer Name: {self.customer_name}")
+        print(f"Account Number: {self.account_number}")
+        print(f"Routing Number: {self.routing_number}")
         print(f"Current Balance: ${self.current_balance}")
         print(f"Minimum Balance: ${self.minimum_balance}")
         print("--------------------------------")
-
-
-account1 = BankAccount("Jim John", 1000, 100)
-account2 = BankAccount("Sarah Madison", 5000, 500)
-
-# Print customer information
-account1.print_customer_information()
-account2.print_customer_information()
-
-# Testing deposit method
-account1.deposit(500)
-account2.deposit(1000)
-
-# Testing withdraw method with valid withdrawal
-account1.withdraw(200)
-account2.withdraw(2000)
-
-# Testing withdraw method with validation
-account1.withdraw(1500)
-account2.withdraw(3600)
