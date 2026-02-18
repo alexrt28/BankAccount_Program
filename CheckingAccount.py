4bbf31b369a11ac33e3501810d5ee1a9d0fc275a
Julian Dominguez <jdominguez9@gmail.com>
2026-02-18 16:04:23 -0500

    feat: add transfer method to checking, create checking accounts in main

--- CheckingAccount.py
+++ CheckingAccount.py
@@ -4,3 +4,11 @@ class CheckingAccount(BankAccount):
     def __init__(self,customer_name, current_balance, minimum_balance, transfer_limit):
         super.__init__(customer_name,current_balance,minimum_balance)
         self.transfer_limit = transfer_limit
+
+    def transfer(self, amount, destination_account):
+        if amount > self.transfer_limit:
+            print(f"Transfer failed: Transfer amount exceeds transfer limit of ${self.transfer_limit}")
+        else:
+            self.withdraw(amount)
+            destination_account.deposit(amount)
+            print(f"${amount} transferred successfully.")
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-import CheckingAccount
+from CheckingAccount import CheckingAccount
+import SavingsAccount import SavingsAccount
+
+def main():
+
+    checking1 = CheckingAccount("Johnathon", 5000, 500, 400)
+    checking2 = CheckingAccount("Miriam", 12000, 1000, 5000)
+
 
