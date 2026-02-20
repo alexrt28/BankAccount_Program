b6caad9ffbb14e064ce4d4d0ca96f60be6e10858
Alex Roy <alexroy.sebastian@gmail.com>
2026-02-20 18:00:09 -0500

    feat: Added protected and private members to savings account

--- SavingsAccount.py
+++ SavingsAccount.py
@@ -1,7 +1,7 @@
 import BankAccount
 class SavingsAccount(BankAccount):
-    def __init__(self, customer_name, current_balance, minimum_balance, interest_rate):
-        super().__init__(customer_name, current_balance, minimum_balance)
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number, interest_rate):
+        super().__init__(customer_name, current_balance, account_number, routing_number, minimum_balance)
         self.interest_rate = interest_rate
 
     def apply_interest(self):
