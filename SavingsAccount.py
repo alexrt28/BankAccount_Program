211233073904ffea6581231a276abeedfd7489b9
Alex Roy <alexroy.sebastian@gmail.com>
2026-02-20 18:16:01 -0500

    fix: fixing an error in import in the SavingsAccount file

--- SavingsAccount.py
+++ SavingsAccount.py
@@ -1,4 +1,4 @@
-import BankAccount
+from BankAccount import BankAccount
 class SavingsAccount(BankAccount):
     def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number, interest_rate):
         super().__init__(customer_name, current_balance, account_number, routing_number, minimum_balance)
